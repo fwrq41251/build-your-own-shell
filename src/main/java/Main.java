dd9ab8a5931270164020c9eb73dbe39c0595945c
winry <fwrq41251@gmail.com>
2025-11-05 10:41:05 +0900

    implement exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,37 @@ public class Main {
     try (var scanner = new Scanner(System.in)) {
       while (true) {
         System.out.print("$ ");
-        var command = scanner.nextLine();
+        var command = parse(scanner.nextLine());
+        run(command);
+      }
+    }
+  }
+
+  enum CommandType {
+    exit, unknown
+  }
+
+  record Command(CommandType command, int arg) {
+  }
+
+  private static Command parse(String command) {
+    var args = command.split(" ");
+    var arg = Integer.parseInt(args[1]);
+    var commandType = CommandType.valueOf(args[0]);
+    if (commandType == null) {
+      commandType = CommandType.unknown;
+    }
+    return new Command(commandType, arg);
+  }
+
+  private static void run(Command command) {
+    switch (command.command) {
+      case unknown -> {
         var error = String.format("%s: command not found", command);
         System.out.println(error);
+      }
+      case exit -> {
+        System.exit(command.arg);
       }
     }
   }
