2f4c5f96bb573986f572007f0e10621b5ba23733
winry <fwrq41251@gmail.com>
2025-11-05 10:26:34 +0900

    repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    System.out.print("$ ");
     try (var scanner = new Scanner(System.in)) {
-      var command = scanner.nextLine();
-      var error = String.format("%s: command not found", command);
-      System.out.println(error);
+      while (true) {
+        System.out.print("$ ");
+        var command = scanner.nextLine();
+        var error = String.format("%s: command not found", command);
+        System.out.println(error);
+      }
     }
   }
 }
